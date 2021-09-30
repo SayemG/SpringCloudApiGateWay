f7a301a85bb77017fefb8cc35e1f8224efa15e03
Sayem Chowdhury <fr9838@wayne.edu>
2021-09-29 20:29:30 -0400

    Initial commit

---
+++ src/main/java/com/microservice/photoapp/springcloudgateway/springcloudapigateway/SpringCloudApiGateWayApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.photoapp.springcloudgateway.springcloudapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudApiGateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudApiGateWayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservice/photoapp/springcloudgateway/springcloudapigateway/SpringCloudApiGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.photoapp.springcloudgateway.springcloudapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudApiGateWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
